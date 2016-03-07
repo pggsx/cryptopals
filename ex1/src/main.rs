0fb20b4fc973baea9e9dd603934140bde963048a
Pavan Gudimetta <pgudimetta@gmail.com>
2016-03-06 19:32:28 -0500

    update

--- ex1/src/main.rs
+++ ex1/src/main.rs
@@ -1,18 +1,18 @@
 use std::io;
-
+use std::io::prelude::*;
 fn main() {
-	println!("CryptoPals Rust");
-	println!("Enter Hex Number:");
-	let mut hex_input = String::new();	
-	io::stdin().read_line(&mut hex_input)
-	.ok()
-	.expect("IO Error Unable to read line");
-	print!("Input was {}",hex_input);
-	let bytes = hex_input.as_bytes();
-	for b in bytes{
-		println!("{:x}",b);
-	}
-	//for a in hex_input.chars(){
-	//println!("{} char",a);
-	//}
+    println!("CryptoPals Rust");
+    println!("Enter Hex Number:");
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(n) => {
+            println!("{} bytes read", n);
+            //   println!("{}", input);
+        }
+        Err(error) => println!("error: {}", error),
+    }
+
+    for a in input.chars(){
+        println!("{} char",a);
+    }
 }
